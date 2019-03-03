36aeda62748cc83dbb67008462e254dffb88df09
飞 左 <348068347@qq.com>
2019-03-03 12:04:28 +0800

    Initial commit

---
+++ src/main/java/com/jili/hadmin/HadminApplication.java
@@ -0,0 +1,13 @@
+package com.jili.hadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HadminApplication.class, args);
+    }
+
+}
