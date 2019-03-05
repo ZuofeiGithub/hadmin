44402775e52ef2be72980d781ac19af2f7f88e53
飞 左 <348068347@qq.com>
2019-03-05 20:38:16 +0800

    admin界面设计

--- src/main/resources/static/custom/js/index.js
+++ src/main/resources/static/custom/js/index.js
@@ -1,6 +1,42 @@
-layui.use(['layer', 'form'], function(){
+layui.use(['layer','element','form'], function(){
     var layer = layui.layer
         ,form = layui.form;
+    var element = layui.element;
+    var $ = layui.$
 
-    layer.msg('Hello World');
+    var isShow = true;  //定义一个标志位
+    $('.kit-side-fold').click(function(){
+        //选择出所有的span，并判断是不是hidden
+        $('.layui-nav-item span').each(function(){
+            if($(this).is(':hidden')){
+                $(this).show();
+            }else{
+                $(this).hide();
+            }
+        });
+        //判断isshow的状态
+        if(isShow){
+            $('.layui-side.layui-bg-black').width(60); //设置宽度
+            $('.kit-side-fold i').css('margin-right', '70%');  //修改图标的位置
+            //将footer和body的宽度修改
+            $('.layui-body').css('left', 60+'px');
+            $('.layui-footer').css('left', 60+'px');
+            //将二级导航栏隐藏
+            $('dd span').each(function(){
+                $(this).hide();
+            });
+            //修改标志位
+            isShow =false;
+        }else{
+            $('.layui-side.layui-bg-black').width(200);
+            $('.kit-side-fold i').css('margin-right', '10%');
+            $('.layui-body').css('left', 200+'px');
+            $('.layui-footer').css('left', 200+'px');
+            $('dd span').each(function(){
+                $(this).show();
+            });
+            isShow =true;
+        }
+    });
 });
+
