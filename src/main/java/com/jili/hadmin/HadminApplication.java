313a5c093e34bd69bbec78d219be95152774a2eb
Administrator <348068347@qq.com>
2019-03-07 21:32:23 +0800

    dao包扫描配置

--- src/main/java/com/jili/hadmin/HadminApplication.java
+++ src/main/java/com/jili/hadmin/HadminApplication.java
@@ -1,8 +1,10 @@
 package com.jili.hadmin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value = "com.jili.hadmin.dao")
 @SpringBootApplication
 public class HadminApplication {
 
