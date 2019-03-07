0af3d156087b949cfbe9463474ea838708705540
zuofei <348068347@qq.com>
2019-03-07 14:10:39 +0800

    后台框架重新书写

---
+++ src/main/java/com/jili/hadmin/constant/URL.java
@@ -0,0 +1,25 @@
+package com.jili.hadmin.constant;
+
+/**
+ * @Author: 左飞
+ * @Date: 2019/3/7 13:21
+ * @Version 1.0
+ */
+public class URL {
+    private static final String LYINGPREFIX = "lying/html/";
+    public static final String ADMIN = LYINGPREFIX + "index.html";
+    public static final String LOGIN = LYINGPREFIX + "login.html";
+    public static final String ABOUT = LYINGPREFIX + "about.html";
+    public static final String CONSOLE = LYINGPREFIX + "console.html";
+    public static final String FORGOT = LYINGPREFIX + "forgot.html";
+    public static final String ERROR = LYINGPREFIX + "error.html";
+    public static final String REGISTER = LYINGPREFIX + "register.html";
+
+    private static final String DOCPREFIX = "lying/doc/";
+    public static final String CHANGELOG = DOCPREFIX + "changelog.html";
+    public static final String LICENSE = DOCPREFIX + "license.html";
+    public static final String MENU = DOCPREFIX + "menu.html";
+    public static final String OTHER = DOCPREFIX + "other.html";
+    public static final String QUICKSTART = DOCPREFIX + "quickstart.html";
+    public static final String TABS = DOCPREFIX + "tabs.html";
+}
--- src/main/java/com/jili/hadmin/controller/FrontController.java
+++ src/main/java/com/jili/hadmin/controller/FrontController.java
@@ -1,18 +1,80 @@
 package com.jili.hadmin.controller;
 
+import com.jili.hadmin.constant.URL;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * 前端页面
+ */
 @Controller
 public class FrontController {
 
     @GetMapping(value = "admin")
     public String index(){
-        return "index.html";
+        return URL.ADMIN;
     }
-
     @GetMapping(value = "login")
     public String login(){
-        return "login.html";
+        return URL.LOGIN;
     }
+
+    @GetMapping(value = "about")
+    public String about(){
+        return URL.ABOUT;
+    }
+
+    @GetMapping(value = "error")
+    public String error(){
+        return URL.ERROR;
+    }
+
+    @GetMapping(value = "console")
+    public String console(){
+        return URL.CONSOLE;
+    }
+
+    @GetMapping(value = "register")
+    public String register(){
+        return URL.REGISTER;
+    }
+
+    @GetMapping(value = "forgot")
+    public String forgot(){
+        return URL.FORGOT;
+    }
+
+    /**
+     *  文档
+     */
+    @GetMapping(value = "changelog")
+    public String changelog(){
+        return URL.CHANGELOG;
+    }
+
+    @GetMapping(value = "license")
+    public String license(){
+        return URL.LICENSE;
+    }
+
+    @GetMapping(value = "menu")
+    public String menu(){
+        return URL.MENU;
+    }
+
+    @GetMapping(value = "other")
+    public String other(){
+        return URL.CHANGELOG;
+    }
+
+    @GetMapping(value = "quickstart")
+    public String quickstart(){
+        return URL.QUICKSTART;
+    }
+
+    @GetMapping(value = "tabs")
+    public String tabs(){
+        return URL.TABS;
+    }
+
 }
