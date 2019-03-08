d3b9772e94106c01b3cf986b97d9acafece11d8f
zuofei <348068347@qq.com>
2019-03-08 17:33:35 +0800

    添加admin用户

--- src/main/java/com/jili/hadmin/author/UserRealm.java
+++ src/main/java/com/jili/hadmin/author/UserRealm.java
@@ -7,11 +7,16 @@ import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -58,7 +63,20 @@ public class UserRealm extends AuthorizingRealm {
         System.out.println("认证逻辑");
         //1.判断用户名
         UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
-        SysUser sysUser = sysUserService.queryByUserName(token.getUsername());
+        String userName = token.getUsername();
+        //处理session
+        DefaultWebSecurityManager securityManager = (DefaultWebSecurityManager) SecurityUtils.getSecurityManager();
+        DefaultWebSessionManager sessionManager = (DefaultWebSessionManager)securityManager.getSessionManager();
+        Collection<Session> sessions = sessionManager.getSessionDAO().getActiveSessions();//获取当前已登录的用户session列表
+        for(Session session:sessions){
+            //清除该用户以前登录时保存的session
+            if(userName.equals(String.valueOf(session.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY)))) {
+                sessionManager.getSessionDAO().delete(session);
+            }
+        }
+
+
+        SysUser sysUser = sysUserService.queryByUserName(userName);
         if(ObjectUtils.isEmpty(sysUser))
         {
             return null;
