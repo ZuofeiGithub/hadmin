4522b3e5be67d1de50fec1b48483b10caafc82e7
zuofei <348068347@qq.com>
2019-03-13 16:56:55 +0800

    树形表单完成

--- src/main/java/com/jili/hadmin/controller/SysMenuController.java
+++ src/main/java/com/jili/hadmin/controller/SysMenuController.java
@@ -2,6 +2,7 @@ package com.jili.hadmin.controller;
 
 import com.jili.hadmin.entity.SysMenu;
 import com.jili.hadmin.json.ErrorMsg;
+import com.jili.hadmin.json.TreeTableData;
 import com.jili.hadmin.service.RoleUserService;
 import com.jili.hadmin.service.SysMenuService;
 import org.apache.shiro.SecurityUtils;
@@ -47,20 +48,50 @@ public class SysMenuController {
     @GetMapping("getMenuList")
     public Object getMenuList(){
         List<Object> list = new ArrayList<>();
-        List<SysMenu> sysMenus = new ArrayList<>();
-
-        Subject subject = SecurityUtils.getSubject();
-        if(subject.hasRole("ROLE_ADMIN")){
-            sysMenus = sysMenuService.getMenuList(1);
-        }else if(subject.hasRole("ROLE_USER")){
-            sysMenus = sysMenuService.getMenuList(2);
-        }
+        List<SysMenu> sysMenus = getSysMenus();
         if(ObjectUtils.isEmpty(sysMenus)){
             list.add(new ArrayList<>());
         }
         list.add(sysMenus);
 
         return list;
+    }
+
+    @GetMapping("gettreeMenuList")
+    public Object gettreeMenuList(){
+        List<SysMenu> sysMenus = getSysMenus();
+        List<TreeTableData> dataList = new ArrayList<>();
+        for(SysMenu sysMenu:sysMenus){
+            TreeTableData data = new TreeTableData();
+            data.setId(sysMenu.getId());
+            data.setPid(sysMenu.getParentId());
+            data.setTitle(sysMenu.getTitle());
+            data.setUrl(sysMenu.getHref());
+            if(sysMenu.getList().size() > 0){
+                for(SysMenu child:sysMenu.getList()){
+                    TreeTableData childdata = new TreeTableData();
+                    childdata.setId(child.getId());
+                    childdata.setTitle(child.getTitle());
+                    childdata.setPid(child.getParentId());
+                    childdata.setUrl(child.getHref());
+                    dataList.add(childdata);
+                }
+            }
+            dataList.add(data);
+        }
+        return dataList;
+    }
+
+    private List<SysMenu> getSysMenus() {
+        List<SysMenu> sysMenus = new ArrayList<>();
+
+        Subject subject = SecurityUtils.getSubject();
+        if (subject.hasRole("ROLE_ADMIN")) {
+            sysMenus = sysMenuService.getMenuList(1);
+        } else if (subject.hasRole("ROLE_USER")) {
+            sysMenus = sysMenuService.getMenuList(2);
+        }
+        return sysMenus;
     }
 
     @PostMapping("addMenu")
---
+++ src/main/java/com/jili/hadmin/json/TreeTableData.java
@@ -0,0 +1,49 @@
+package com.jili.hadmin.json;
+
+public class TreeTableData {
+
+    /**
+     * id : 1
+     * pid : 0
+     * title : 1-1
+     */
+
+    private int id;
+    private int pid;
+    private String title;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
